5039f552e373e862a620777435380ef06ac5b50c
joylibingyao <joylibingyao@gmail.com>
2015-12-06 17:46:34 -0500

    Initial Commit

---
+++ HW4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW4
+//
+//  Created by Bingyao Li on 12/6/15.
+//  Copyright © 2015 CMPSC122. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
